c30b385976379e571e20afea7a70268df1b10a28
Boris Kuchin <bvkuchin@gmail.com>
2022-07-16 23:44:01 +0300

    added dynamic user list

--- src/main/java/ru/bvkuchin/networkchat/ChatDialogController.java
+++ src/main/java/ru/bvkuchin/networkchat/ChatDialogController.java
@@ -2,17 +2,29 @@ package ru.bvkuchin.networkchat;
 
 import javafx.application.Platform;
 import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
 import javafx.fxml.FXML;
-import javafx.scene.control.*;
-import javafx.scene.input.MouseButton;
+import javafx.scene.control.Button;
+import javafx.scene.control.ListView;
+import javafx.scene.control.MenuItem;
+import javafx.scene.control.TextArea;
 import javafx.scene.input.MouseEvent;
 import ru.bvkuchin.networkchat.components.Connection;
 
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
-public class HelloController {
+public class ChatDialogController {
+
+    private static final String AUTH_CMD_PREFIX = "/auth";
+    private static final String AUTHOK_CMD_PREFIX = "/authok";
+    private static final String AUTHERR_CMD_PREFIX = "/autherr";
+    private static final String CLIENT_MSG_CMD_PREFIX = "/cMsg";
+    private static final String SERVER_MSG_CMD_PREFIX = "/sMsg";
+    private static final String PRIVATE_MSG_CMD_PREFIX = "/pm";
+    private static final String STOP_SERVER_CMD_PREFIX = "/stop";
+    private static final String END_CLIENT_CMD_PREFIX = "/end";
+    private static final String LIST_CLIENTS_CMD_PREFIX = "/usrs";
+
 
     SimpleDateFormat dateFormat = new SimpleDateFormat("EEE, MMM d, ''yy HH:mm:ss");
 
@@ -68,10 +80,10 @@ public class HelloController {
     @FXML
     void initialize() {
 
-        usersList.getItems().add("Жорик");
-        usersList.getItems().add("Толик");
-        usersList.getItems().add("Жмурик");
-        usersList.getItems().add("Дурик");
+//        usersList.getItems().add("Жорик");
+//        usersList.getItems().add("Толик");
+//        usersList.getItems().add("Жмурик");
+//        usersList.getItems().add("Дурик");
 
 
        /* usersList.setOnMouseClicked(new EventHandler<MouseEvent>() {
@@ -95,27 +107,32 @@ public class HelloController {
 
     public void getMessageFromInputStream() {
         Thread t = new Thread(() -> {
+            try {
+                while (true) {
+                    String message = connection.readMessage();
 
-                try {
-                    while (true) {
-                        String message = connection.readMessage();
+                    switch (message.split("\\s+", 2)[0]) {
+                        case LIST_CLIENTS_CMD_PREFIX:
+                            Platform.runLater(() -> {
+                                usersList.getItems().clear();
+                                usersList.getItems().addAll(message.split("\\s+", 2)[1].trim().split("\\s+"));
+                            });
+                            break;
+                        default:
                             Platform.runLater(() -> {
                                 convHistoryList.getItems().add(message);
                                 convHistoryList.scrollTo(convHistoryList.getItems().size());
                             });
                     }
-                } catch (Exception e) {
-                    System.out.println("Соединение разорвано сервером. Перезапустите сервер и подключитесь снова");
-                    e.printStackTrace();
                 }
+            } catch (Exception e) {
+                System.out.println("Соединение разорвано сервером. Перезапустите сервер и подключитесь снова");
+                e.printStackTrace();
+            }
         });
 
-
         t.start();
-
-
     }
-
 
     private void clearInputField() {
         textEnterField.setText("");
